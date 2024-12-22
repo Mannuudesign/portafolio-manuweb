80a60e8609edfbfc5ffd45153f9de216e31f945e
Ü <128203194+Mannuudesign@users.noreply.github.com>
2024-12-22 12:09:56 -0400

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
       <footer className="bg-black text-white text-center py-4 mt-4">
-        <p>ManüWebDesign © 2023 All rights reserved</p>
+        <p>ManüWebDesign © 2024-2025 All rights reserved</p>
       </footer>
     );
   }
